ea3a23783a2e8c6c5393fe2f68426ec01d5f634c
DerickLFAndrade <derickspy15@gmail.com>
2021-11-01 04:31:53 -0300

    criação do  cart

--- index.js
+++ index.js
@@ -3,31 +3,35 @@ function consumo() {
 
     fetch("https://raw.githubusercontent.com/owInteractive/desafio-frontend-2020/master/produtos.json").then(response => {
 
-        return response.json()
+        return response.json();
 
     }).then(body => {
         let index;
         
         const listarProdutos = () => {
+            
             body.map(item => {
-                const card = document.createElement('div')
-
+                const input = document.querySelector('[data-pesquisa]');
+                const table = document.querySelector('[data-table]');
+                const card = document.createElement('div');
+                const button = document.createElement('button');
                 const criarCard = () => {
-                    const table = document.querySelector('[data-table]')
                    
-                    card.className = 'card'
-                    card.style.display = 'flex'
-                    card.style.justifyContent = 'center'
-                    card.style.alignItems = 'center'
-                    card.style.border = '2px solid rgba(0, 0, 0, 0.1)'
+                   
+                    card.className = 'card';
+                    card.id = 'card'
+                    card.style.display = 'flex';
+                    card.style.justifyContent = 'center';
+                    card.style.alignItems = 'center';
+                    card.style.border = '2px solid rgba(0, 0, 0, 0.1)';
 
-                    card.style.marginLeft = '50px'
-                    card.style.marginTop = '50px'
+                    card.style.marginLeft = '50px';
+                    card.style.marginTop = '50px';
                    
 
-                    let cardImg = document.createElement('img')
-                    cardImg.className = 'card-img-top'
-                    cardImg.src = item.imageUrl
+                    let cardImg = document.createElement('img');
+                    cardImg.className = 'card-img-top';
+                    cardImg.src = item.imageUrl;
                     
 
 
@@ -35,64 +39,61 @@ function consumo() {
 
 
 
-                    let cardBody = document.createElement('div')
-                    cardBody.className = 'card-body'
+                    let cardBody = document.createElement('div');
+                    cardBody.className = 'card-body';
+                    
 
-                    let categ = document.createElement('p')
-                    categ.className = 'card-title'
-                    categ.style.fontWeight = 'bold'
-                    categ.style.color = 'blueviolet'
-                    categ.innerHTML = item.category
-                    cardBody.appendChild(categ)
+                    let categ = document.createElement('p');
+                    categ.className = 'card-title';
+                    categ.style.fontWeight = 'bold';
+                    categ.style.color = 'blueviolet';
+                    categ.innerHTML = item.category;
+                    cardBody.appendChild(categ);
 
-                    let titul = document.createElement('h4')
-                    titul.className = 'titulo-prod'
-
-
-                    titul.innerHTML = item.name
-                    cardBody.appendChild(titul)
-
-                    let desc = document.createElement('p')
-                    desc.className = 'card-text'
-                    desc.style.fontSize = '8px'
-                    desc.style.color = '#808080'
-                    desc.innerHTML = 'Descrição padrão para todos os produtos'
-                    cardBody.appendChild(desc)
-
-                    let priceC = document.createElement('h4')
-                    priceC.className = 'card-text'
-                    priceC.style.fontSize = '15px'
-                    priceC.style.fontWeight = 'bold'
-                    priceC.innerHTML = `${item.price.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' })}`
-                    cardBody.appendChild(priceC)
+                    let titul = document.createElement('h4');
+                    titul.className = 'titulo-prod';
 
 
+                    titul.innerHTML = item.name;
+                    cardBody.appendChild(titul);
+
+                    let desc = document.createElement('p');
+                    desc.className = 'card-text';
+                    desc.style.fontSize = '8px';
+                    desc.style.color = '#808080';
+                    desc.innerHTML = 'Descrição padrão para todos os produtos';
+                    cardBody.appendChild(desc);
+
+                    let priceC = document.createElement('h4');
+                    priceC.className = 'card-text';
+                    priceC.style.fontSize = '15px';
+                    priceC.style.fontWeight = 'bold';
+                    priceC.innerHTML = `${item.price.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' })}`;
+                    cardBody.appendChild(priceC);
 
 
+                    
+                    button.className = 'button';
+                    button.textContent = 'ADICIONAR AO CARRINHO';
 
+                    button.style.fontWeight = 'bold';
+                    button.style.border = 'none';
+                    button.style.borderTop = '2px solid rgba(0, 0, 0, 0.1)';
 
-                    let button = document.createElement('button')
-                    button.className = 'button'
-                    button.textContent = 'ADICIONAR AO CARRINHO'
+                    cardBody.appendChild(button);
+                    cardBody.style.display = 'flex';
+                    cardBody.style.justifyContent = 'center';
+                    cardBody.style.flexDirection = 'column';
+                    card.appendChild(cardImg);
+                    card.appendChild(cardBody);
 
-                    button.style.fontWeight = 'bold'
-                    button.style.border = 'none'
-                    button.style.borderTop = '2px solid rgba(0, 0, 0, 0.1)'
-
-                    cardBody.appendChild(button)
-                    cardBody.style.display = 'flex'
-                    cardBody.style.justifyContent = 'center'
-                    cardBody.style.flexDirection = 'column'
-                    card.appendChild(cardImg)
-                    card.appendChild(cardBody)
-
-                    table.appendChild(card)
+                    table.appendChild(card);
 
                 }
 
-                const armazenarNome = () => {
-                    const input = document.querySelector('[data-pesquisa]')
-                    const icoP = document.querySelector('[data-ico-p]')
+                const pesquisar = () => {
+                    
+                    const icoP = document.querySelector('[data-ico-p]');
 
                     let filtroNome;
                     filtroNome = item.name.toUpperCase();
@@ -106,13 +107,13 @@ function consumo() {
                     input.addEventListener('keydown', (e) => {
                         if (e.key === 'Enter') {
 
-                            index = filtroNome.indexOf(inps)
+                            index = filtroNome.indexOf(inps);
                             
                             if (index == -1) {
 
-                                card.style.display = 'none'
+                                card.style.display = 'none';
                             } else {
-                                card.style.display = 'block'
+                                card.style.display = 'block';
                             } 
                         }
                     })
@@ -122,16 +123,35 @@ function consumo() {
 
                         if (index == -1) {
 
-                            card.style.display = 'none'
+                            card.style.display = 'none';
                         } else {
-                            card.style.display = 'block'
-                        }
-                    })
+                            card.style.display = 'block';
+                        };
+                    });
 
 
                 }
+               
+                const adicionarCarrinho = () => {
+                    const cont_p = document.querySelector('[data-cont-p]')
+                  const ico_cart = document.querySelector('[data-ico-cart]')
+                    const cart_mod = document.querySelector('[data-cart]')
+                    ico_cart.addEventListener("click", () => {
+                        cont_p.style.display = 'none';
+                        cart_mod.style.display = 'block';
+                        table.style.display = 'none';
+                        cont_p.style.display = 'none';
+                       
+                       
+                    })
+                }
+
+                adicionarCarrinho();
+               
                 criarCard();
-                armazenarNome();
+                pesquisar();
+
+              
             })
 
 
@@ -143,9 +163,7 @@ function consumo() {
 
 
 
-        const adicionarCarrinho = () => {
-
-        }
+      
 
         listarProdutos();
         console.log(index)
