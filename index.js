7083b30b9224067a5deb5c686a0cd904ad89e2aa
DerickLFAndrade <derickspy15@gmail.com>
2021-11-02 07:23:37 -0300

    funcionalidades cart

--- index.js
+++ index.js
@@ -7,9 +7,12 @@ function consumo() {
 
     }).then(body => {
         let index;
-        
+     
+        let contador_cart = document.querySelector('[data-contador-cart]');
         const listarProdutos = () => {
-            
+           
+            let quantCar = 0;
+            let SomaTotal = 0;
             body.map(item => {
                 const input = document.querySelector('[data-pesquisa]');
                 const table = document.querySelector('[data-table]');
@@ -17,8 +20,8 @@ function consumo() {
                 const button = document.createElement('button');
                 input.focus();
                 const criarCard = () => {
-                   
-                   
+
+
                     card.className = 'card';
                     card.id = 'card'
                     card.style.display = 'flex';
@@ -28,12 +31,12 @@ function consumo() {
 
                     card.style.marginLeft = '50px';
                     card.style.marginTop = '50px';
-                   
+
 
                     let cardImg = document.createElement('img');
                     cardImg.className = 'card-img-top';
                     cardImg.src = item.imageUrl;
-                    
+
 
 
 
@@ -42,7 +45,7 @@ function consumo() {
 
                     let cardBody = document.createElement('div');
                     cardBody.className = 'card-body';
-                    
+
 
                     let categ = document.createElement('p');
                     categ.className = 'card-title';
@@ -73,7 +76,7 @@ function consumo() {
                     cardBody.appendChild(priceC);
 
 
-                    
+
                     button.className = 'button';
                     button.textContent = 'ADICIONAR AO CARRINHO';
 
@@ -93,7 +96,7 @@ function consumo() {
                 }
 
                 const pesquisar = () => {
-                    
+
                     const icoP = document.querySelector('[data-ico-p]');
 
                     let filtroNome;
@@ -109,18 +112,18 @@ function consumo() {
                         if (e.key === 'Enter') {
 
                             index = filtroNome.indexOf(inps);
-                            
+
                             if (index == -1) {
 
                                 card.style.display = 'none';
                             } else {
                                 card.style.display = 'block';
-                            } 
+                            }
                         }
                     })
                     icoP.addEventListener('click', () => {
                         let index;
-                        index = filtroNome.indexOf(inps)
+                        index = filtroNome.indexOf(inps);
 
                         if (index == -1) {
 
@@ -132,29 +135,157 @@ function consumo() {
 
 
                 }
-               
-                const adicionarCarrinho = () => {
-                    const cont_p = document.querySelector('[data-cont-p]')
-                  const ico_cart = document.querySelector('[data-ico-cart]')
-                    const cart_mod = document.querySelector('[data-cart]')
+
+                const adicionarCarrinho = () => {  
+                    const cont_p = document.querySelector('[data-cont-p]');
+                    const ico_cart = document.querySelector('[data-ico-cart]');
+                    const cart_mod = document.querySelector('[data-cart]');
+                    
+                    
+                   const msg = document.querySelector('[data-msg]')
+
+
+
+
+                    //REPLICAR PRODUTOS NO CARRINHO
+                    
+                    const carrinho = document.querySelector('[data-carrinho-produto]');
+
+
+                    const soma = document.querySelector('[data-soma]');
+                    const fimCart = document.querySelector('[data-fim-cart]');
+                    
+
+
+                    const containerClone = document.querySelector("[data-prod-clone]");
+
+                    const novoProd = containerClone.cloneNode(true);
+                   
+
+                    novoProd.style.display = '';
+                   
+
+                    const descItens = document.querySelector('[data-desc-itens]')
+
+                  
+                    let quantCarInt = 0;
+
+                    button.addEventListener('click', () => {
+                        descItens.style.display = ''
+                        msg.style.display = 'none';
+                        soma.style.display = '';
+                        fimCart.style.display = '';
+                        contador_cart.style.display = ''
+                        
+                        const categoria = novoProd.querySelector('[data-categoria]'); //1°
+                        const nomeProd = novoProd.querySelector('[data-nome-prod]'); //2°
+                        const contCart = novoProd.querySelector('[data-cont-cart-2]'); //3°
+                        const valorUni = novoProd.querySelector('[data-valor-un]'); //4°
+                        const dividido1 = novoProd.querySelector('[data-10x-1]'); //5°
+                        const valorTot = novoProd.querySelector('[data-valor-tot]');//6°
+                        const dividido2 = novoProd.querySelector('[data-10x-2]'); //7°
+                       
+
+                        const avista = document.querySelector('[data-avista]')//8°
+                        const parcelado = document.querySelector('[data-parcelado]')
+                        const totalCartao = document.querySelector('[data-total-pag]')
+
+                        categoria.innerHTML = item.category;
+                       nomeProd.innerHTML = item.name;
+
+                        //contador externo total
+                        contador_cart.innerHTML = quantCar + 1;
+                        quantCar++;
+
+                       //quantidade no carrinho (botão interno)
+                       contCart.innerHTML = quantCarInt + 1;
+                        quantCarInt++;
+
+
+                       //valor unitário
+                        valorUni.innerHTML = (item.price).toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' });
+
+                        //valor dividido 1
+                        dividido1.innerHTML = `10X ${(item.price / 10).toLocaleString('pt-BR', {style: 'currency', currency: 'BRL'})}`;
+
+                       
+
+                        //valor total
+                       valorTot.innerHTML = (item.price * quantCarInt).toLocaleString('pt-BR', {style: 'currency', currency: 'BRL'});
+                        //valor dividido 1
+                        dividido2.innerHTML = `10X ${((item.price * quantCarInt ) / 10).toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' })}`;
+
+                        SomaTotal += item.price;    
+                    
+                        //pagamento total à vista
+                        avista.innerHTML = SomaTotal.toLocaleString('pt-BR', {style:'currency', currency: 'BRL'});
+
+                        //pagamento total parcelado
+                        parcelado.innerHTML = `10x de${(SomaTotal/10).toLocaleString('pt-BR', {style: 'currency', currency: 'BRL'})}`;
+
+                        //pagamento total 
+                        totalCartao.innerHTML = `(${SomaTotal.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' })})`;
+
+                        
+                       carrinho.appendChild(novoProd);
+                     
+                     
+                        
+                       
+                    })
+                    const bot_cont_cp = document.querySelector('[data-bot-cp]');
+
+                    bot_cont_cp.addEventListener('click', () => {
+                        cont_p.style.display = '';
+                        cart_mod.style.display = 'none';
+                        table.style.display = '';
+                       
+                    })
+
+                    //abrir carrinho
                     ico_cart.addEventListener("click", () => {
                         cont_p.style.display = 'none';
-                        cart_mod.style.display = 'block';
+                        cart_mod.style.display = '';
                         table.style.display = 'none';
                         cont_p.style.display = 'none';
-                       
-                       
                     })
                 }
 
                 adicionarCarrinho();
-               
+
                 criarCard();
                 pesquisar();
 
-              
+
             })
 
+            const anima_Nav = () =>{
+                const nav = document.querySelector('[data-nav]')
+                const navFk = document.querySelector('[data-nav-fk]')
+                const logo1 = document.querySelector('[data-logo-1]')
+                document.onscroll = () => {
+
+                    if (scrollY > 310) {
+                        navFk.style.display = '';
+                        contador_cart.style.color = '#fff'
+                        nav.classList.add('anima');
+                        nav.style.position = 'fixed';
+                        nav.style.background = 'rgba(0, 0, 0, 0.4)';
+                        nav.style.height = '60px';
+                        logo1.style.color = 'white';
+                    } else {
+                        logo1.style.color = 'rgb(153, 153, 153)';
+                        contador_cart.style.color = 'red'
+                        nav.style.height = '70px';
+                        navFk.style.display = 'none';
+                        nav.classList.remove('anima');
+                        nav.classList.add('anima2');
+                        nav.style.position = '';
+                        nav.style.background = '#fff';
+                    }
+                }
+            }
+           anima_Nav();
 
 
 
@@ -164,14 +295,14 @@ function consumo() {
 
 
 
-      
+
 
         listarProdutos();
         console.log(index)
 
     });
 
-  
+
 
 }
 consumo()
